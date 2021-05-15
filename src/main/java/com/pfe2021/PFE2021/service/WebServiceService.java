ffa7f55e1985e71dcfaa1bf26a8b23b501ac4487
Malek Toujani <maliktoujani@gmail.com>
2021-05-15 16:06:35 +0100

    fusion des deux packages webservice et webservices

--- src/main/java/com/pfe2021/PFE2021/service/WebServiceService.java
+++ src/main/java/com/pfe2021/PFE2021/service/WebServiceService.java
@@ -1,22 +1,32 @@
 package com.pfe2021.PFE2021.service;
 
 import com.pfe2021.PFE2021.exceptions.SolutionPartenaireNotFoundException;
-import com.pfe2021.PFE2021.model.WebService;
+import com.pfe2021.PFE2021.model.*;
+import com.pfe2021.PFE2021.repository.SolutionPartenaireRepository;
 import com.pfe2021.PFE2021.repository.WebServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @Transactional
 public class WebServiceService {
     private final WebServiceRepository webServiceRepository;
+    private final SolutionPartenaireRepository solutionPartenaireRepository;
+    private final HistoriqueAppelService historiqueAppelService;
 
-    @Autowired
-    public WebServiceService (WebServiceRepository webServiceRepository){
+    private RestTemplate restTemplate=new RestTemplate();
+
+    public WebServiceService(WebServiceRepository webServiceRepository, SolutionPartenaireRepository solutionPartenaireRepository, HistoriqueAppelService historiqueAppelService) {
         this.webServiceRepository = webServiceRepository;
+        this.solutionPartenaireRepository = solutionPartenaireRepository;
+        this.historiqueAppelService = historiqueAppelService;
     }
 
     public List<WebService> findAllWebServices(){
@@ -38,4 +48,43 @@ public class WebServiceService {
     public void deleteWebService(Long id){
         webServiceRepository.deleteWebServiceById(id);
     }
+
+    public Object findAll(Long id) {
+        WebService webService = webServiceRepository.findWebServiceById(id).get();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+
+        SolutionPartenaire solutionPartenaire = solutionPartenaireRepository.findSolutionPartenaireByUsername(currentPrincipalName).get();
+
+        LocalDateTime now = LocalDateTime.now();
+
+        Object[] objects = null;
+
+        for(Contrat c : solutionPartenaire.getContrats()){
+            for(InfoAcces ia : c.getInfoAcces()){
+                if(ia.getWebService().equals(webService)) {
+                    for (PeriodeAcces pa : ia.getPeriodeAcces()) {
+                        if (pa.getJour().equalsIgnoreCase(now.getDayOfWeek().toString())) {
+                            if (pa.getHeureDebut() <= now.getHour() && pa.getHeureFin() > now.getHour()) {
+                                objects = restTemplate.getForObject(webService.getUrl(), Object[].class);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        HistoriqueAppel historiqueAppel = new HistoriqueAppel();
+
+        historiqueAppel.setDateHeure(now);
+        historiqueAppel.setResultat(objects!=null);
+        historiqueAppel.setSolutionPartenaire(solutionPartenaire);
+        historiqueAppel.setWebService(webService);
+
+        this.historiqueAppelService.addHistoriqueAppel(historiqueAppel);
+
+        return objects;
+    }
+
 }
